a11a60fc5b9de50b676588eb0016a59dd47c118c
Никита <nnaumov2394@gmail.com>
2026-07-02 17:10:38 +0300

    Temporary open admin

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\QueueBoardController;
 use App\Http\Controllers\Admin\QueueController;
 use App\Http\Controllers\Admin\UserController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,7 +18,30 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/*
+|--------------------------------------------------------------------------
+| ВРЕМЕННО БЕЗ АВТОРИЗАЦИИ (для демонстрации)
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/admin/queue', [QueueController::class, 'index'])->name('admin.queue.index');
+Route::post('/admin/queue/{ticket}/call', [QueueController::class, 'call'])->name('admin.queue.call');
+Route::post('/admin/queue/{ticket}/complete', [QueueController::class, 'complete'])->name('admin.queue.complete');
+Route::delete('/admin/queue/{ticket}', [QueueController::class, 'destroy'])->name('admin.queue.destroy');
+
+Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
+Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
+Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('admin.users.update');
+Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
+
+/*
+|--------------------------------------------------------------------------
+| Пользователь
+|--------------------------------------------------------------------------
+*/
+
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -27,16 +51,6 @@ Route::middleware('auth')->group(function () {
     Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
     Route::post('/appointments/{appointment}/cancel', [AppointmentController::class, 'cancel'])->name('appointments.cancel');
     Route::get('/appointments/{appointment}/ticket', [AppointmentController::class, 'ticket'])->name('appointments.ticket');
-
-    Route::get('/admin/queue', [QueueController::class, 'index'])->name('admin.queue.index');
-    Route::post('/admin/queue/{ticket}/call', [QueueController::class, 'call'])->name('admin.queue.call');
-    Route::post('/admin/queue/{ticket}/complete', [QueueController::class, 'complete'])->name('admin.queue.complete');
-    Route::delete('/admin/queue/{ticket}', [QueueController::class, 'destroy'])->name('admin.queue.destroy');
-
-    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
-    Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
-    Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('admin.users.update');
-    Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
 });
 
 require __DIR__.'/auth.php';
