59a7a39362aa2980a7c563e7a205e641e3941d3b
Никита <nnaumov2394@gmail.com>
2026-07-02 17:06:19 +0300

    Fix admin routes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\QueueBoardController;
 use App\Http\Controllers\Admin\QueueController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,6 +26,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/appointments/create', [AppointmentController::class, 'create'])->name('appointments.create');
     Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
     Route::post('/appointments/{appointment}/cancel', [AppointmentController::class, 'cancel'])->name('appointments.cancel');
+    Route::get('/appointments/{appointment}/ticket', [AppointmentController::class, 'ticket'])->name('appointments.ticket');
 
     Route::get('/admin/queue', [QueueController::class, 'index'])->name('admin.queue.index');
     Route::post('/admin/queue/{ticket}/call', [QueueController::class, 'call'])->name('admin.queue.call');
@@ -37,10 +37,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
     Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('admin.users.update');
     Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
-    Route::get('/admin/login', [AuthController::class, 'showLogin'])->name('admin.login');
-    Route::post('/admin/login', [AuthController::class, 'login'])->name('admin.login.post');
-    Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
-    Route::get('/appointments/{appointment}/ticket', [AppointmentController::class, 'ticket'])->name('appointments.ticket');
 });
 
 require __DIR__.'/auth.php';
